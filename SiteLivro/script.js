cb67039f0d815e64ff6fe60a916798c2605f3dd7
JoaoBozz <163414633+JoaoBozz@users.noreply.github.com>
2025-12-03 12:47:33 -0300

    terminei site

--- SiteLivro/script.js
+++ SiteLivro/script.js
@@ -12,5 +12,65 @@ const observer = new IntersectionObserver((entries) => {
     threshold: 0.3 // 20% visível na tela
 });
 
+const palavras = ["Dedicada", "Atenciosa", "Experiente", "Inspiradora"];
+let i = 0;
+const h1 = document.getElementById("troca");
+
+setInterval(() => {
+  // sai
+  h1.classList.remove("entrando");
+  h1.classList.add("saindo");
+
+  setTimeout(() => {
+    // troca palavra
+    i = (i + 1) % palavras.length;
+    h1.textContent = palavras[i];
+
+    // entra
+    h1.classList.remove("saindo");
+    h1.classList.add("entrando");
+  }, 600); // mesmo tempo do transition
+}, 2000);
+
+const modal = document.getElementById("modalPersonagem");
+const modalTitulo = document.getElementById("modalTitulo");
+const modalTexto = document.getElementById("modalTexto");
+const fechar = document.querySelector(".fechar");
+
+// Seleciona todos os cards
+document.querySelectorAll(".cardPersonagem").forEach(card => {
+    card.addEventListener("click", () => {
+
+        const nome = card.querySelector("h1").innerText;
+
+        // Texto placeholder gigante (troque pelo seu depois)
+        const textoGrande = `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Suspendisse potenti. Mauris nec justo vel leo consequat
+        faucibus. Sed sit amet ipsum a urna pulvinar feugiat.
+        Vivamus tincidunt nulla at ipsum elementum, in commodo
+        velit venenatis. Cras porta sem at arcu consequat,
+        sed sodales lectus gravida. Aenean ac nisl non nunc 
+        fermentum fermentum. (COLOQUE SEU TEXTO AQUI)
+        `.repeat(8); // deixa grande
+
+        modalTitulo.innerText = nome;
+        modalTexto.innerText = textoGrande;
+
+        modal.style.display = "flex";
+    });
+});
+
+// Fechar no X
+fechar.onclick = () => {
+    modal.style.display = "none";
+};
+
+// Fechar clicando fora da caixa
+modal.onclick = (e) => {
+    if (e.target === modal) modal.style.display = "none";
+};
+
+
 // Seleciona tudo com a classe reveal
 document.querySelectorAll(".reveal").forEach(el => observer.observe(el));
